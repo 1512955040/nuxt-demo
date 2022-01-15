18e9c471fb1e6ee997bdd4f5e5812bc45cca1668
1512955040 <liu13116464360@163.com>
2022-01-15 21:30:57 +0800

    04-动态路由

---
+++ pages/user/_id.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>User Page</h1>
+</template>
+
+<script>
+export default {
+    name:'UserPage',
+    created(){
+        //创建下划线_id.vue （备注：必须叫_id.vue） 比如user/_id.vue 访问浏览器则可传递参数 user/3
+        console.log(this.$route.params.id)
+    }
+}
+</script>
+
+<style>
+
+</style>
