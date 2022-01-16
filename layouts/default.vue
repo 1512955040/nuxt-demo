592d67488437858013913598285836dd453039fe
1512955040 <liu13116464360@163.com>
2022-01-16 12:12:24 +0800

    08-视图 布局

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <!-- 目的是设置公共部分Layout组件，是一个更简便的嵌套路由，公共部分可以设置在这块 -->
+        <h1>layouts/default.vue</h1>
+        <!-- 页面出口，类似于子路由出口 -->
+        <nuxt/>    
+    </div>
+</template>
+
+<script>
+export default {
+    name:'LayoutDefault'
+}
+</script>
+
+<style>
+
+</style>
---
+++ layouts/foo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <h1>我是foo，index.vue中默认是layout中的default组件，也可以自己设置比如foo</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
